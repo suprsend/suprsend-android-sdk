d6aac876da054b96afe3aff91f547df9c0588e6e
ssniks <nikhilesh@suprsend.com>
2022-06-10 19:34:04 +0530

    Adding staging url in repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
         mavenCentral()
         maven(url = "https://kotlin.bintray.com/kotlinx")
         maven(url = "https://maven.google.com")
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.0")
@@ -23,6 +24,7 @@ allprojects {
         google()
         mavenCentral()
         jcenter() // Warning: this repository is going to shut down soon
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
